8cf26c33d3c6aef9062ed7e3d8e75defe41e5464
Lea <leawork25@gmail.com>
2025-07-07 13:10:27 -0800

    update 7/6

--- src/pages/Demographics/Demographics.jsx
+++ src/pages/Demographics/Demographics.jsx
@@ -2,7 +2,83 @@ import React from 'react'
 
 const Demographics = () => {
   return (
-    <div>Demographics</div>
+    <div>
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__title">A.I. ANALYSIS</div>
+          <h1 className='header__title'>DEMOGRAPHICS</h1>
+            <p className='header__para'>A.I. HAS ESTIMATED THE FOLLOWING. <br />FIX ESTIMATED INFORMATION IF NEEDED. 
+            </p>
+        </div>
+
+        <div className="container__wrapper--left">
+          <div className="container__one">
+            <div className="text__results">EAST ASIAN</div>
+            <div className="text__race">RACE</div>
+          </div>
+          <div className="container__two">
+            <div className="text__results">20-29</div>
+            <div className="text__age">AGE</div>
+          </div>
+          <div className="container__three">
+            <div className="text__results">FEMALE</div>
+            <div className="text__sex">SEX</div>
+          </div>
+        </div>
+
+        <div className="container__wrapper--middle">
+          <div className="result__text">East Asian</div>
+          <div className="circle__wrapper">
+            <div className="circle">
+              <div className="percentage">96%</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container__wrapper--right">
+          <div className="header__results--wrapper">
+            <div className="left__title">RACE</div>
+            <div className="right__title">A.I. CONFIDENCE</div>
+            <div className="main__results--wrapper">
+              <ul className='results__list'>
+                <li className="result__wrapper">
+                  <div className="result">East Asian</div>
+                  <div className="percentage">96%</div>
+                </li>
+                <li className="result__wrapper">
+                  <div className="result">White</div>
+                  <div className="percentage">6%</div>
+                </li>
+                <li className="result__wrapper">
+                  <div className="result">Black</div>
+                  <div className="percentage">3%</div>
+                </li>
+                <li className="result__wrapper">
+                  <div className="result">South Asian</div>
+                  <div className="percentage">2%</div>
+                </li>
+                <li className="result__wrapper">
+                  <div className="result">Latino Hispanic</div>
+                  <div className="percentage">0%</div>
+                </li>
+                <li className="result__wrapper">
+                  <div className="result">South East Asian</div>
+                  <div className="percentage">0%</div>
+                </li>
+                <li className="result__wrapper">
+                  <div className="result">Middle Eastern</div>
+                  <div className="percentage">0%</div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="button__wrapper">
+          <button className="reset">RESET</button>
+          <button className="confirm">CONFIRM</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React from 'react';
+import '../Results/Results.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
 
 const Results = () => {
+  const navigate = useNavigate()
+
+  function handleDemographics() {
+    navigate('/Demographics')
+  }
+
+  function handleBack() {
+    navigate('/Analysis')
+  }
+
+  function handleProceed() {
+    navigate('/Demographics')
+  }
+
   return (
-    <div>Results</div>
+    <>
+        <div className='page'>
+            <div className="header__title">A.I. ANALYSIS</div>
+            <p className='header__para'>A.I. HAS ESTIMATED THE FOLLOWING. <br />FIX ESTIMATED INFORMATION IF NEEDED. 
+            </p>
+            <div className="intro__wrapper">
+                <div className="rectangle__wrapper">
+                    <div className="rectangle__one">
+                    <div className="rectangle__two">
+                        <div className="rectangle__three">
+                        </div>
+                        
+                    </div>
+                    </div>
+                    <div className="inner__wrapper">
+                            <button className="inner__square--one" onClick={handleDemographics}>
+                              <div className="inner__text">DEMOGRAPHICS</div>
+                                </button>
+                            <button className="inner__square--two" >
+                              <div className="inner__text">SKIN TYPE DETAILS</div>
+                                </button>
+                            <button className="inner__square--three" >
+                              <div className="inner__text">COSMETIC CONCERNS</div>
+                              </button>
+                            <button className="inner__square--four" >
+                              <div className="inner__text">WEATHER</div>
+                              </button>
+                          </div>
+                </div>
+            </div> 
+          <button className="button__wrapper--left" onClick={handleBack}>
+                <div className="rectangle__small">
+                    <FontAwesomeIcon icon={faCaretLeft} className='icon'/>
+                </div>
+                <div className="text__small">BACK</div>
+            </button>
+            
+                <button className="button__wrapper--right" onClick={handleProceed} >
+                <div className="rectangle__small">
+                    <FontAwesomeIcon icon={faCaretRight} className='icon'/>
+                </div>
+                <div className="text__small">PROCEED</div>
+            </button>
+            
+        </div>
+        </>
   )
 }
 
